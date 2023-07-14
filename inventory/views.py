eaf0bbdfe3522eb4ae1c87670c2957741da9f7ff
Wambui-Mwangi <mwangiestherwambuii@gmail.com>
2023-07-14 15:44:18 +0300

    created product upload form and a view for it

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .forms import ProductUploadForm
+
+#there are class based views and function based views
 
 # Create your views here.
+def upload_product(request):                      #the request represents a http request
+    form = ProductUploadForm()
+    return render(request, "inventory/product_upload.html", {"form": form})
